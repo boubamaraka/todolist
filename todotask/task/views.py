b061da33f59ee37f904be98e28c19e34be40b6eb
boubacuppla <ibrahima@cupplatech.com>
2019-07-08 10:50:50 +0300

    display list view of task added

--- todotask/task/views.py
+++ todotask/task/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Task
+from django.shortcuts import render
 
 # Create your views here.
+def task_list(request):
+    all_task = Task.objects.all()
+    return render(request, 'task/list.html', {'all_task': all_task})

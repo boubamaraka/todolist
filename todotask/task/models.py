7a4a276b182deabb5c2b03c4449e997abf4b5db5
boubacuppla <ibrahima@cupplatech.com>
2019-07-08 10:26:43 +0300

    Task model created

--- todotask/task/models.py
+++ todotask/task/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Task(models.Model):
+    name = models.CharField(max_length=120)
+    creation_date = models.DateTimeField(default=timezone.now())
+    modification_time = models.DateTimeField(default=timezone.now())
+    completed_time = models.DateTimeField(blank=True, null=True)
+    status = models.BooleanField(default=False)
